ffe77edeb3de0a0ba43183ef2400023d0ea2f2f0
Xi Tan <mintancy@hotmail.com>
2023-02-08 14:39:10 -0500

    minor

--- tools/run_script_win.sh
+++ tools/run_script_win.sh
@@ -6,14 +6,17 @@ python -m ectf_tools build.tools --design ..\2023-ectf-UB-Cacti-design\ --name u
 # 3.2.3 Build Deployment
 python -m ectf_tools build.depl --design ..\2023-ectf-UB-Cacti-design\ --name ub_test --deployment ub_depl
 # 3.2.4 Build Car and Paired Fob
-python -m ectf_tools build.car_fob_pair  --design ..\2023-ectf-UB-Cacti-design --name ub_test --deployment ub_depl --car-out car_out --fob-out fob_out --car-name car_ub --fob-name fob_ub --car-id 1 --pair-pin 123456 --car-unlock-secret "Hello UB!" --car-feature1-secret "secret 1" --car-feature2-secret "secret 2" --car-feature3-secret "secret 3"
+python -m ectf_tools --debug build.car_fob_pair --design ..\2023-ectf-UB-Cacti-design --name ub_test --deployment ub_depl --car-out car_out --fob-out fob_out --car-name car_ub --fob-name fob_ub --car-id 1 --pair-pin 123456 --car-unlock-secret "Hello UB!" --car-feature1-secret "secret 1" --car-feature2-secret "secret 2" --car-feature3-secret "secret 3"
 
 # 3.2.5 Build Unpaired Fob
-python -m ectf_tools build.fob --design ..\2023-ectf-UB-Cacti-design\ --name ub_test --deployment ub_depl --fob-out fob_out --fob-name fob_ub
+python -m ectf_tools --debug build.fob --design ..\2023-ectf-UB-Cacti-design\ --name ub_test --deployment ub_depl --fob-out fob_out --fob-name fob_ub_unparied
 
 # 3.3 Load Device
 # before loading the device, use uniflash to load bootloader to the board first
-python -m ectf_tools device.load_hw --dev-in car_out --dev-name car_ub --dev-serial COM4
+python -m ectf_tools device.load_hw --dev-in car_out --dev-name car_ub --dev-serial COM9
+# load unparied fob
+python -m ectf_tools device.load_hw --dev-in fob_out --dev-name fob_ub_unparied --dev-serial COM9
+
 
 # 3.4 Start Bridge
 python3 -m ectf_tools device.bridge --bridge-id 2233 --dev-serial COM3
