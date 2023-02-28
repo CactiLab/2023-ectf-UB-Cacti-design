bc538730673333ba16f294e20e1982a11721e522
Andy Ma <philoround@gmail.com>
2023-02-28 17:10:37 -0500

    add uart init

--- car/src/uart.c
+++ car/src/uart.c
@@ -59,6 +59,11 @@ void uart_init(void)
     UARTConfigSetExpClk(
         UART0_BASE, SysCtlClockGet(), 115200,
         (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    while (UARTCharsAvail(UART0_BASE))
+    {
+        UARTCharGet(UART0_BASE);
+    }
 }
 
 /**
